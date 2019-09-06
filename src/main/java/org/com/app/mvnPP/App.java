964a927d0ae46abff49b5dc1b7767292f0c463a3
mohan <mohan.chanti4@gmail.com>
2019-09-06 12:09:51 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnPP/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnPP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnPP" );
+    }
+}
